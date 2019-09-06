06ee5dc8d90c59dfeadc202364c8dc6f62dbcee3
Sourav Sarker <souravs@mun.ca>
2019-09-06 00:55:50 -0230

    Updated hold button functionality

--- app.js
+++ app.js
@@ -41,6 +41,87 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
     } else {
         // Proceed to nex player
         activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
-    }
+        roundScore = 0;
+        document.getElementById('current-0').textContent = '0';
+        document.getElementById('current-1').textContent = '0';
 
+        document.querySelector('.player-0-panel').classList.toggle('active');
+        document.querySelector('.player-1-panel').classList.toggle('active');
+
+        document.querySelector('.dice').style.display = 'none';
+    }
+    
 })
+
+// Hold button 
+console.log(activePlayer)
+
+document.querySelector('.btn-hold').addEventListener('click', function() {
+    // Add current score to global score
+    
+    scores[activePlayer] += roundScore;
+    document.getElementById('score-' + activePlayer).textContent = scores[activePlayer];
+    roundScore = 0;
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+
+    // if player own the game
+   
+})
+
+
+
+// var scores, roundScore, activePlayer;
+
+// scores = [0, 0];
+// roundScore = 0;
+// activePlayer = 0;
+
+// //making everything default
+
+// document.getElementById('score-0').textContent = '0';
+// document.getElementById('score-1').textContent = '0';
+// document.getElementById('current-0').textContent = '0';
+// document.getElementById('current-1').textContent = '0';
+// document.querySelector('.dice').style.display = 'none';
+
+// // Roll Dice Button
+
+// document.querySelector('.btn-roll').addEventListener('click', function() {
+//     // Generate a random number
+//     var dice = Math.floor(Math.random() * 6) + 1;
+
+//     //Display the random number with dice picture
+//     document.getElementById('current-' + activePlayer).textContent = dice;
+
+//     var diceDom = document.querySelector('.dice');
+//     diceDom.src = 'dice-' + dice + '.png';
+//     document.querySelector('.dice').style.display = 'block';
+
+
+//     // If dice rolled to 1, current score will be 0;
+//     if (dice !==1) {
+//         // if dice is not 1, then add the score
+//         roundScore += dice;
+//         document.getElementById('current-' + activePlayer).textContent = roundScore;
+//     } else {
+//         // make current score 0 and change player
+//         document.querySelector('.dice').style.display = 'none';
+//         activePlayer === 0 ? activePlayer = 1: activePlayer = 0;
+//         roundScore = 0;
+//         document.getElementById('current-0').textContent = '0';
+//         document.getElementById('current-1').textContent = '0';
+
+//         document.querySelector('.player-0-panel').classList.toggle('active');
+//         document.querySelector('.player-1-panel').classList.toggle('active');
+//     }
+// })
+
+// // Hold Button for holding score for player before getting 0;
+
+// document.querySelector('.btn-hold').addEventListener('click', function() {
+//     // Current Score to the global score
+
+// })
+
+
